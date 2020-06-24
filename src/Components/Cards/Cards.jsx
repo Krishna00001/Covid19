03ab2637d090e90c837803f97c8dcdaf4f85310c
Krishna00001 <65583487+Krishna00001@users.noreply.github.com>
2020-06-24 09:27:30 +0530

    Recreated the cards component using Antd

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,48 +1,50 @@
+
+//Done with antd
+
+import { Card, Col, Row } from 'antd';
 import React from 'react';
 import styles from './Cards.module.css';
-import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 import CountUp from 'react-countup';
+import { Typography } from 'antd';
 import cx from 'classnames';
+import Paragraph from 'antd/lib/skeleton/Paragraph';
+const { Title} = Typography;
+
 
 const Cards = ({data: { confirmed, recovered, deaths, lastUpdate }}) => {
     if(!confirmed){
         return 'Loading....';
     }
-    return(
-        <div className = {styles.container}>
-          <Grid container spacing = {3} justify = 'center'>
-                <Grid item component = {Card} xs={12} md={3} className={cx (styles.card, styles.infected)}>
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                        <Typography variant="h5">
-                            <CountUp  start={0} end={confirmed.value} duration={2.5} separator=","/>
-                            </Typography>
-                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-                        <Typography variant="body2">Number of active cases of covid19</Typography>
-                    </CardContent>
-                </Grid>
-                <Grid item component = {Card} xs={12} md={3}  className={cx (styles.card, styles.recovered)}>
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-                        <Typography variant="h5">
-                            <CountUp  start={0} end={recovered.value} duration={2.5} separator=","/>
-                        </Typography>
-                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-                        <Typography variant="body2">Number of recoveries from covid19</Typography>
-                    </CardContent>
-                </Grid>
-                <Grid item component = {Card} xs={12} md={3}  className={cx (styles.card, styles.deaths)}>
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-                        <Typography variant="h5">
-                            <CountUp  start={0} end={deaths.value} duration={2.5} separator=","/>
-                        </Typography>
-                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-                        <Typography variant="body2">Number of deaths caused by covid19</Typography>
-                    </CardContent>
-                </Grid>          
-            </Grid>
-        </div>
+return (
+
+      <div>
+        <Row justify='center' className={styles.container}>
+          <Col>
+            <Card className={cx (styles.card, styles.infected)}>
+            <Title level={1} >Infected</Title>
+             <CountUp  start={0} end={confirmed.value} duration={2.5} separator=","/> 
+             <p>{new Date(lastUpdate).toDateString()}</p>
+             <p>Number of active cases of covid19</p>
+            </Card>
+          </Col>
+          <Col>
+            <Card className={cx (styles.card, styles.recovered)}>
+            <Title level={1} >Recovered</Title>
+            <CountUp  start={0} end={recovered.value} duration={2.5} separator=","/>
+            <p>{new Date(lastUpdate).toDateString()}</p>
+            <p>Number of recoveries from covid19</p>
+            </Card>
+          </Col>
+          <Col>
+            <Card className={cx (styles.card, styles.deaths)}>
+            <Title level={1} >Deaths</Title>
+            <CountUp  start={0} end={deaths.value} duration={2.5} separator=","/> 
+            <p>{new Date(lastUpdate).toDateString()}</p>
+            <p>Number of deaths caused by covid19</p>
+            </Card>
+          </Col>
+        </Row>
+      </div>
     )
 }
 
